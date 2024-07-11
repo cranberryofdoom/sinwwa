d7d87504568582fd32c5731eb7adc1b408c0788a
Ashley Qian <ashley.qian.0@gmail.com>
2024-07-10 21:01:06 -0700

    chore: clean up tab stories

--- src/tabs/Tabs.stories.tsx
+++ src/tabs/Tabs.stories.tsx
@@ -1,14 +1,18 @@
 import type { Meta, StoryFn } from "@storybook/react";
 import { Tabs } from "./Tabs";
 import { useState } from "react";
+import { Tab } from "./Tab";
 
-const meta: Meta<typeof Tabs> = {
+const meta: Meta<typeof Tabs.Root> = {
   title: "Tabs/Tabs",
+  component: Tabs.Root,
+  tags: ["autodocs"],
+  subcomponents: { Tab } as {},
 };
 
 export default meta;
 
-export const Example: StoryFn = () => {
+export const Example: StoryFn<typeof Tabs.Root> = () => {
   const [selectedTab, setSelectedTab] = useState("tab1");
   return (
     <Tabs.Root>
